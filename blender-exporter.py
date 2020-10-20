9c52c6febdff9ea4a0c1c610191d2323c7840f3e
linusmossberg <linusmossbergmt@gmail.com>
2020-10-20 15:54:02 +0200

    Update blender script

--- blender-exporter.py
+++ blender-exporter.py
@@ -2,6 +2,12 @@ import bpy
 import os
 import copy
 
+extent_x = 500
+num_x = 27
+num_y = 27
+
+baseline = extent_x / (num_x - 1) # millimeters
+
 scene = bpy.context.scene
 
 camera = scene.camera
@@ -10,11 +16,8 @@ center_pos = copy.deepcopy(camera.location)
 initial_seed = copy.deepcopy(scene.cycles.seed);
 right, up, back = camera.matrix_world.to_3x3().transposed()
 
-extent_x = 4000
-num_x = 101
-num_y = 63
-
-baseline = extent_x / (num_x - 1) # millimeters
+render_filepath = copy.deepcopy(scene.render.filepath)
+directory = os.path.dirname(render_filepath)
 
 for j in range(num_y):
     for i in range(num_x):
@@ -22,11 +25,12 @@ for j in range(num_y):
         v = (j - (num_y - 1) / 2) * baseline;
         camera.location = center_pos + right * u * 1e-3 + up * v * 1e-3;
         
-        name = "%s_%02d_%02d_%d_%d_%d_%d" % (camera.name, j, i, -v, u, camera.data.lens, camera.data.sensor_width)
-        scene.render.filepath = os.path.join("C:/tmp", name)
+        name = "%s_%02d_%02d_%f_%f_%d_%d" % (camera.name, j, i, -v, u, camera.data.lens, camera.data.sensor_width)
+        scene.render.filepath = os.path.join(directory, name)
         
         scene.cycles.seed = i * num_x + j;
         bpy.ops.render.render(write_still=True)
 
 scene.cycles.seed = initial_seed
+scene.render.filepath = render_filepath
 camera.location = center_pos
